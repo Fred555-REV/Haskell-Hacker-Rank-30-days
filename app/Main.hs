95d9b92670a1954b9541c8642a42c2b02274f8b8
Freddy Canido <freddy.canido12@gmail.com>
2021-08-10 16:05:04 -0400

    did day 1 in haskell but can't get import to work in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
 import Lib
+import Day1
 
 main :: IO ()
-main = someFunc
+main = day1
+  -- putStrLn "Select day 1-30"
+  -- selection <- getLine
+  -- let input = read selection::Int
+  -- if input == 1
+  --   then day1
+  -- else  print "Hmmm"
---
+++ src/Day1.hs
@@ -0,0 +1,25 @@
+module Day1
+    ( day1
+    ) where
+
+day1 :: IO ()
+day1 = do
+  let num = 4
+  let fpn = 4.0
+  let str = "HackerRank "
+
+  print "Enter integer: "
+  inputNum <- getLine
+  let x = read inputNum::Integer
+
+  print "Enter floating point number: "
+  inputFpn <- getLine
+  let y = read inputFpn::Float
+
+  print "Enter String: "
+  inputStr <- getLine
+  let str2 = inputStr::String
+
+  print (num+x)
+  print (fpn+y)
+  print (str++str2)
