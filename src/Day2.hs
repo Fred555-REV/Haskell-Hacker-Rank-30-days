205b8a924950e95453be7acc5b152433543953db
Freddy <freddy.canido12@gmail.com>
2021-08-11 22:40:29 -0400

    added input for day2 1-5 done for now

--- src/Day2.hs
+++ src/Day2.hs
@@ -4,4 +4,15 @@ module Day2
 
 calculateMealCost x y z = round(x + ((x*y)/100) + ((x*z)/100))
 day2 :: IO ()
-day2 = print "not done"
+day2 = do
+  print "Enter meal cost: "
+  x <- getLine
+  print "Enter tax percent: " 
+  y <- getLine
+  print "Enter tip percent: "
+  z <- getLine
+  let cost = read x::Double
+  let tax = read y::Double
+  let tip = read z::Double
+  print "The total meal cost is: "
+  print (calculateMealCost cost tax tip)
