8a0c9da130e42769bf3ffb2801e3136ba9c67183
Freddy Canido <freddy.canido12@gmail.com>
2021-08-12 17:35:14 -0400

    Cleaned up day6 but no multiple string inputs

--- src/Day6.hs
+++ src/Day6.hs
@@ -5,37 +5,20 @@ module Day6
 part1 x = [ x !! y | y <- [0..length x - 1], odd y]
 part2 x = [ x !! y | y <- [0..length x - 1], even y]
 wholeOutput x = (part2 x) ++ " " ++ (part1 x)
--- getInput =
---   do
---   x <- getLine
---   let str = read x::String
---   return wholeOutput str
+
+repeatNTimes 0 _ = return ()
+repeatNTimes n action =
+ do
+  action
+  repeatNTimes (n-1) action
+
 day6 :: IO ()
 day6 = do
   print "Enter amount of strings: "
-  -- num <- getLine
-  -- let amount = read num::Int
+  num <- getLine
+  let amount = read num::Int
 
-  -- x <- getLine
-  -- let str = read x::String
-  -- print wholeOutput x
-
-  -- print wholeOutput getLine
-
-  -- print "2"
-  -- print [wholeOutput x | x <- getLine, i <- [1..amount]]
-  -- print [wholeOutput getLine | i <- [1..amount]]
-  -- print [getInput | i <- [1..amount]]
-  -- return "2"
-
-
-  -- print (take amount (repeat (getMultOutputs)))
-
--- getMultOutputs :: IO ()
--- getMultOutputs = do
---   x <- getLine
---
---   -- print wholeOutput x
---
---   let str = read x::String
---   return wholeOutput str
+  print "Enter string: "
+  input <- getLine
+  let output = wholeOutput input
+  print output
